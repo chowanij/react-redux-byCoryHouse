634a3f7433c95d01b3cadf522f8a131dd5684d76
chowanij <jan.chowaniec@outlook.com>
2016-10-23 13:10:37 +0200

    - Courses form

--- src/components/course/CourseList.js
+++ src/components/course/CourseList.js
@@ -2,16 +2,9 @@ import React, {PropTypes} from 'react';
 import CourseListRow from './CourseListRow';
 
 const CourseList = ({courses}) => {
-  const isTrue = true;
   return(
     <table className="table">
       <thead>
-      {isTrue &&
-      (
-        <tr>
-          <th colspan="5"> COURSES TABLE </th>
-       </tr>
-      )}
       <tr>
         <th>&nbsp;</th>
         <th>Title</th>
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,27 +2,45 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
-class ManageCoursePage extends React.Component{
+class ManageCoursePage extends React.Component {
 
-  constructor(props, context){
+  constructor(props, context) {
     super(props, context);
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors: {}
+    };
+
   }
 
-  render(){
-    return(
-      <h1>Manage Course</h1>
+  render() {
+    return (
+        <CourseForm
+          allAuthors={[]}
+          course={this.state.course}
+          errors={this.state.errors}
+        />
     );
   }
 }
 
-ManageCoursePage.PropTypes = {
-  //myProp: PropTypes.string.isRequired
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired
 };
 
-function mapStateToProps(state, ownProps){
-  return{
-    state: state
+function mapStateToProps(state, ownProps) {
+  let course = {
+    id: '',
+    watchHref: '',
+    title: '',
+    authorId: '',
+    length: '',
+    category: ''
+  };
+  return {
+    course: course
   };
 }
 
